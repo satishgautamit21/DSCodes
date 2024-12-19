f9a0d79f0d1f11e69991f7ac1a7dfbb87de3af71
Satish Gautam <satish.gautam@turnberrysolutions.com>
2024-12-19 20:17:23 +0530

    good works

---
+++ getAllDuplicate.js
@@ -0,0 +1,12 @@
+function getAllDuplicates(nums) {
+    const seen = new Set();
+    const duplicates = [];
+    for (let num of nums) {
+        if (seen.has(num)) {
+            duplicates.push(num);
+        } else {
+            seen.add(num);
+        }    
+    }
+    return duplicates;
+}
