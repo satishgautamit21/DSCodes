0ff1813484fb80e4b9f811735f0ef8ee7bc6880c
Satish Gautam <satish.gautam@turnberrysolutions.com>
2024-12-19 18:32:02 +0530

    good works

--- powerOfTwo.js
+++ powerOfTwo.js
@@ -1,16 +1,16 @@
-// function powerOfTwo(n) {
-//     return n > 0 && (n & (n - 1)) === 0;
-// }
-
 function powerOfTwo(n) {
-    while(n > 1) {
-        if(n % 2 !== 0) {
-            return false;
-        }
-        n /= 2;
-    }
-    return true;
+    return n > 0 && (n & (n - 1)) === 0;
 }
+
+// function powerOfTwo(n) {
+//     while(n > 1) {
+//         if(n % 2 !== 0) {
+//             return false;
+//         }
+//         n /= 2;
+//     }
+//     return true;
+// }
 
 /* 
     Solution one has time complexity of O(1) and space complexity of O(1) 
