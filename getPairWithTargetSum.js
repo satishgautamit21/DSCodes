42f64af5e436ca95ebd194befea3845c98e5b29e
Satish Gautam <satish.gautam@turnberrysolutions.com>
2024-12-19 19:56:38 +0530

    good works

--- getPairWithTargetSum.js
+++ getPairWithTargetSum.js
@@ -1,10 +1,21 @@
+// function getPairWithTargetSum(array, targetSum) {
+//     const seen = {}
+//     for (let i = 0; i < array.length; i++) {
+//         const complement = targetSum - array[i];
+//         if (seen[complement]) {
+//             return [complement, array[i]];
+//         }
+//         seen[array[i]] = true;
+//     }
+// }
+
 function getPairWithTargetSum(array, targetSum) {
-    const seen = {}
+    const seen = new Set();
     for (let i = 0; i < array.length; i++) {
         const complement = targetSum - array[i];
-        if (seen[complement]) {
+        if (seen.has(complement)) {
             return [complement, array[i]];
         }
-        seen[array[i]] = true;
+        seen.add(array[i]);
     }
 }
