f1a25aee9d914ab6deb3aceb6f644e9f0be62464
Satish Gautam <satish.gautam@turnberrysolutions.com>
2024-12-20 13:37:04 +0530

    good works

---
+++ slidingWindow/smllestSubArray.js
@@ -0,0 +1,17 @@
+function smallestSubArray(nums, target) {
+    let sum = 0;
+    let minLen = Infinity;
+    let left = 0;
+    for (let right = 0; right < nums.length; right++) {
+        sum += nums[right];
+        while (sum >= target) {
+            minLen = Math.min(minLen, right - left + 1);
+            sum -= nums[left];
+            left++;
+        }
+    }
+    return minLen === Infinity ? 0 : minLen;
+}
+
+// Time complexity: O(n)
+// Space complexity: O(1)
