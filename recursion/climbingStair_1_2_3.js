6b84e0d07e9d62c8d930f4d9283d9c667637b6e2
Satish Gautam <satish549321@gmail.com>
2025-02-19 19:20:23 +0530

    new code changes added

---
+++ recursion/climbingStair_1_2_3.js
@@ -0,0 +1,31 @@
+function climbStairs(n) {
+    if (n === 1) {
+        return 1;
+    }
+    if (n === 2) {
+        return 2;
+    }
+    if (n === 3) {
+        return 4;
+    }
+    return climbStairs(n - 1) + climbStairs(n - 2) + climbStairs(n - 3);
+}
+
+/*
+   function climbStairs(n) {
+    if (n === 1) {
+        return 1;
+    }
+    if (n === 2) {
+        return 2;
+    }
+    if (n === 3) {
+        return 4;
+    }
+    let res = [1, 2, 4];
+    for(let i = 4; i <= n; i++) {
+        res[i] = res[i - 1] + res[i - 2] + res[i - 3];
+    }
+    return res[n];
+} 
+*/
