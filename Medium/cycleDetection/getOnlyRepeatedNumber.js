a243ec7fb9ad4d9770401f22566a2e3047755a8a
Satish Gautam <satish549321@gmail.com>
2025-02-26 16:07:04 +0530

    new code changes added

--- Medium/cycleDetection/getOnlyRepeatedNumber.js
+++ Medium/cycleDetection/getOnlyRepeatedNumber.js
@@ -1,14 +1,15 @@
-// Get repeated number in an array of N+1 integers having digit from 1 to N
+// Get only repeated number in an array of N+1 integers having digit from 1 to N
 
 // We will use floyd's cycle detection algorithm
-function getRepeatedNumber(arr) {
+function getOnlyRepeatedNumber(arr) {
     let slow = arr[0];
     let fast = arr[0];
-
+    console.log("slow, fast   ",slow, fast);
     // this loop will find the starting point of the cycle
     do {
         slow = arr[slow];
         fast = arr[arr[fast]];
+        console.log("slow, fast   ",slow, fast);
     } while (slow !== fast);
 
     // this loop will find the repeated number
@@ -19,3 +20,7 @@ function getRepeatedNumber(arr) {
     }
     return slow;
 }
+
+
+console.log(getOnlyRepeatedNumber([1, 3, 4, 2, 2])); // Output: 2
+// console.log(getOnlyRepeatedNumber([3, 1, 3, 4, 2])); // Output: 3
--- Medium/stack and queue/removeAdjDuplicates.js
+++ Medium/stack and queue/removeAdjDuplicates.js
@@ -1,3 +1,5 @@
+// Problem: https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+
 function removeAdjDuplicates(str) {
     let stack = []
 
