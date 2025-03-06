f559db86a126a42af84aa49a5c7fc4c5cca36a80
Satish Gautam <satish549321@gmail.com>
2025-03-06 23:10:47 +0530

    new code change added

--- Easy/arrays/productExceptSelf.js
+++ Easy/arrays/productExceptSelf.js
@@ -1,5 +1,14 @@
 // https://leetcode.com/problems/product-of-array-except-self/
 
+/*
+    We solve this in two passes:
+
+    First pass: Compute prefix product (product of elements before nums[i]).
+    Second pass: Compute suffix product (product of elements after nums[i]).
+    Multiply prefix and suffix products to get the final answer.
+*/
+
+
 function productExceptSelf(nums) {
 
     // we will create an array of size nums.length and fill it with 1
@@ -16,3 +25,22 @@ function productExceptSelf(nums) {
     }
     return result;
 }
+
+// brute force way
+
+function productExceptSelf(nums) {
+    const result = [];
+    for (let i = 0; i < nums.length; i++) {
+        let product = 1;
+        for (let j = 0; j < nums.length; j++) {
+            if (i !== j) {
+                product *= nums[j];
+            }
+        }
+        result.push(product);
+    }
+    return result;
+}
+
+
+console.log(productExceptSelf([5, 2, 3, 4])); // Output: [24, 12, 8, 6]
--- Medium/slidingWindow/containerWithMostWater.js
+++ Medium/slidingWindow/containerWithMostWater.js
@@ -1,4 +1,22 @@
 // https://leetcode.com/problems/container-with-most-water/
+/*
+You are given an integer array height of length n. There are n vertical lines drawn such that 
+the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container 
+contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+
+
+Example 1:
+
+
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+*/
 function containerWithMostWater(height) {
     let l=0;
     let r=height.length-1;
@@ -10,6 +28,7 @@ function containerWithMostWater(height) {
         area = h*width;
         maxArea = Math.max(maxArea, area);
 
+        // move the smaller height
         if(height[l]<height[r]){
             l++;
         }else{
